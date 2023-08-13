e8b8945506021d844dd7dd2d5da2be4673b1736d
https://github.com/kushpedia/git-test.git <haronkuria2015@gmail.com>
2023-08-13 14:38:33 +0300

    add functionality to NewGame button

--- script.js
+++ script.js
@@ -12,9 +12,24 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-let currentScore = 0;
-let activePlayer = 0;
+let currentScore,activePlayer, playing, score;
+const initFunction = function(){
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    diceEl.classList.add('hidden');
+    score =[0,0];
+    current1El.textContent = 0;
+    current0El.textContent = 0;
+    scoreEl1.textContent = 0;
+    scoreEl0.textContent = 0;
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
 
+};
+initFunction();
 const switchPlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     activePlayer = activePlayer ===0 ? 1:0;
@@ -23,13 +38,12 @@ const switchPlayer = function(){
     player1El.classList.toggle('player--active');
 };
 // create initial state
-scoreEl0.textContent = 0;
-scoreEl1.textContent = 0;
-diceEl.classList.add('hidden');
-let score =[0,0]
+
 // New button functionality
 
 btnRoll.addEventListener('click', function(){
+
+if(playing){
 // 1. roll a random dice
 const dice = Math.trunc(Math.random()*6)+1;
 // console.log(dice);
@@ -43,37 +57,40 @@ if not added to the current points
 if (dice !==1){
     // add score to current score and display
     currentScore += dice;
-
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     
-
 }
 else{
     // switch players
-    switchPlayer();
+    switchPlayer(); 
 
 
 }
-
+}
 });
 
 btnHold.addEventListener('click', function(){
-    // add the activeplayer point to the current player score
+    if(playing){
+     // add the activeplayer point to the current player score
     score[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
+    // console.log(score[activePlayer]);
 
-// check if player work
-if (score[activePlayer] >= 20){
-    document.querySelector(`player--${activePlayer}`).classList.add('player--winner');
-    document.querySelector(`player--${activePlayer}`).classList.remove('player--active');
-}
+// check if player wins
+    if (score[activePlayer] >= 20){
+        playing = false;
+        diceEl.classList.add('hidden');
+        console.log(`${activePlayer} Won`)
 
-
-
-    // switch players
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        
+    }
+    else{
+// switch players
     switchPlayer()
-
-
-
-
+    }
+    }
 });
+
+btnNew.addEventListener('click', initFunction);
